b598f556a80e577830679f69056f8e7736b6cdea
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2022-11-07 11:15:06 +0900

    「TopPage/サインイン/サイナップ」のフロント & 投稿機能サーバーサイド

--- app/controllers/anniversaries_controller.rb
+++ app/controllers/anniversaries_controller.rb
@@ -1,5 +1,23 @@
 class AnniversariesController < ApplicationController
   def index
-    
+    @anniversaries = Anniversary.all
+  end
+
+  def new
+    @anniversary = Anniversary.new
+  end
+
+  def create
+    @anniversary = Anniversary.new(anniversary_params)
+    if @anniversary.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def anniversary_params
+    params.require(:anniversary).permit(:image, :anniversary_name, :anniversary_date, :anniversary_description, :category_id, :who_anniversary).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "anniversaries#index"
-  resources :anniversaries, only: [:index] 
+  resources :anniversaries, only: [:index, :new, :create] 
 end
