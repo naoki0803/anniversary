ab6f548e757a3a113472dcc08c5c05a1296456c2
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2022-11-19 01:47:35 +0900

    本番環境のみCSSが崩れる(https://qiita.com/kazutosato/items/c9dfa99d10411ced64b7)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,11 +10,21 @@ require("channels")
 require('jquery')
 
 
+
 import "bootstrap"
 import "../stylesheets/application"
 
 import '@fortawesome/fontawesome-free/js/all'
 import '../css/application.scss'
+
+
+import jquery from "jquery"
+window.$ = window.jQuery = jquery
+import * as bootstrap from "bootstrap"
+window.bootstrap = bootstrap
+import "../stylesheets/application"
+
+
 
 
 
