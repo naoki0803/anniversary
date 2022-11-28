f5c2beb9e2c1998836b80728ef5d62022c11af65
naoki0803 <110111909+naoki0803@users.noreply.github.com>
2022-11-28 15:33:21 +0900

    TopPageレイアウト修正(hero、ヘッダーメニューのレスポンシブ対応)

--- app/controllers/anniversaries_controller.rb
+++ app/controllers/anniversaries_controller.rb
@@ -2,16 +2,13 @@ class AnniversariesController < ApplicationController
   require 'date'
 
   before_action :authenticate_user!, except: [:index, :show]
-  
+  before_action :all_item, only: [:index, :all]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
-    @anniversaries = Anniversary.all
   end
 
   def all
-    @anniversaries = Anniversary.includes(:user).order("created_at DESC")
-    @comment = Comment.new
-    # @commentいらない
   end
 
   def new
@@ -27,15 +24,23 @@ class AnniversariesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @anniversary.update(anniversary_params)
+      redirect_to anniversary_path(@anniversary.id)
+    else
+      render :edit
+    end
+  end
+
   def show
-    @anniversary = Anniversary.find(params[:id])
     @comment = Comment.new
     @comments = @anniversary.comments.includes(:user).order("created_at DESC")
 
     # @after_100day = @anniversary.created_at.next_day(2)
-    
     # @today = Time.current 
-
     # @countdown = (@after_100day - @today).to_i
 
     # @countdown_sec = (@after_100day - @today).to_i
@@ -44,8 +49,21 @@ class AnniversariesController < ApplicationController
     # @countdown_day = (@after_100day - @today).to_i / 60 / 60 / 24
   end
 
+  def destroy
+    @anniversary.destroy
+    redirect_to user_path(current_user.id)
+  end
+
   private
   def anniversary_params
     params.require(:anniversary).permit(:image, :anniversary_name, :anniversary_date, :anniversary_description, :category_id, :who_anniversary).merge(user_id: current_user.id)
+  end
+
+  def all_item
+    @anniversaries = Anniversary.includes(:user).order("created_at DESC")
+  end
+
+  def set_item
+    @anniversary = Anniversary.find(params[:id])
   end
 end
